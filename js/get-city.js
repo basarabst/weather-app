43155fabd3da9e0e2fac5e21f68d4061eb43f4f4
Stanislav Basarab <stanislavbasarab083@gmail.com>
2023-03-06 09:47:56 +0200

    Remove unused code and fix js module

--- js/get-city.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-const CITIES = [
-  {city: 'Kyiv', latitude: 50.4375, longitude: 30.5},
-  {city: 'Kharkiv', latitude: 50.0, longitude: 36.25},
-];
--- js/index.js
+++ js/index.js
@@ -12,10 +12,10 @@ const btn = document.querySelector('.btn');
 const render = async (city) => {
   const weatherData = await getWeatherData(city);
   for (let i = 0; i < NUM_OF_DAYS; i++) {
-    const date = `<td class="date-col-${i}">${weatherData.dates[i]}</td>`;
+    const date = `<td class="date-col">${weatherData.dates[i]}</td>`;
     dateRow.insertAdjacentHTML('beforeend', date);
 
-    const weather = `<td class="weather-col-${i}">${weatherData.temperatures[i]}</td>`;
+    const weather = `<td class="weather-col">${weatherData.temperatures[i]}</td>`;
     weatherRow.insertAdjacentHTML('beforeend', weather);
   }
 }
